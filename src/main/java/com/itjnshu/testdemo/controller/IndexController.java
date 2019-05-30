9eb4c6abc083e69d927e659d424e96ea8a329955
bitlong <2962584453@qq.com>
2019-05-30 22:15:15 +0800

    new demo test

--- src/main/java/com/itjnshu/testdemo/controller/IndexController.java
+++ src/main/java/com/itjnshu/testdemo/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String hello(){
         return "Index";
     }
 }
